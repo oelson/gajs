34fe73305c2ea672a6c58a34b8a4057ba802e5cb
Nelson Houillon <houillon.nelson@gmail.com>
2020-08-18 22:08:05 +0200

    move to exports & add random lib

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import {join} from 'lodash';
+import { join } from "lodash";
 
 function component() {
-    const element = document.createElement('div');
-  
-    element.innerHTML = join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
-  
+  const element = document.createElement("div");
+
+  element.innerHTML = join(["Hello", "webpack"], " ");
+
+  return element;
+}
+
+document.body.appendChild(component());
