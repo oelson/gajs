06eb784282de247afd02d65e410dc33994dad052
Nelson Houillon <houillon.nelson@gmail.com>
2020-08-19 21:41:59 +0200

    impl presentation

--- src/genetic_algorithm/presentation.js
+++ src/genetic_algorithm/presentation.js
@@ -1,7 +1,18 @@
-export function binary_string(b) {
-  //return ' '.join(bin(x)[2:].zfill(8) for x in b)
+function intToBase(int, base, length) {
+  const coerced = int >>> 0; // https://stackoverflow.com/a/16155417/2950046
+  const binary = coerced.toString(base);
+  const padded = binary.padStart(length, "0");
+  const lengthDiff = padded.length - length;
+  const sized = padded.substring(lengthDiff);
+  return sized;
 }
 
-export function byte_string(b) {
-  // return ' '.join('{:02x}'.format(x) for x in b)
+function binary_string(bytes) {
+  return bytes.map((b) => intToBase(b, 2, 8)).join("");
 }
+
+function byte_string(bytes) {
+  return bytes.map((b) => intToBase(b, 16, 2)).join("");
+}
+
+module.exports = { binary_string, byte_string };
