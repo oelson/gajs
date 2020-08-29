57d70e660bc5c9eb3fc830fed80dd4e5633a4108
Nelson Houillon <houillon.nelson@gmail.com>
2020-08-29 22:07:46 +0200

    enhancements in text.js

--- src/species/text.js
+++ src/species/text.js
@@ -1,46 +1,56 @@
-import { selection } from "./genetic_algorithm/selection";
-import { Being } from "./genetic_algorithm/population";
+const {
+  str_distance,
+  byte_vector_distance,
+} = require("../genetic_algorithm/selection");
+const { Being } = require("../genetic_algorithm/being");
 
-export class TextBeing extends Being {
+class TextBeing extends Being {
   constructor(genotype, encoding) {
+    // TODO encode/decode equivalent
     const phenotype = genotype.decode(encoding, "replace");
     super(genotype, phenotype);
     this.encoding = encoding;
   }
   reproduce(population) {
+    // TODO encode/decode equivalent
     genome_copy = bytearray(this.genotype);
     clone = TextBeing(genome_copy, this.encoding);
     return clone;
   }
 }
 
-export class TextTarget {
+class TextTarget {
   constructor(text, encoding) {
+    // TODO encode/decode equivalent
     genome = bytearray(text.encode(encoding, "replace"));
-    this.being = TextBeing(genome, encoding);
+    this.target = TextBeing(genome, encoding);
   }
   fitness_by_genotype(being) {
-    return selection.bytearray_bit_distance(
-      being.genotype,
-      this.being.genotype
-    );
+    return byte_vector_distance(being.genotype, this.target.genotype);
   }
   fitness_by_phenotype(being) {
-    return selection.letter_distance(being.phenotype, this.being.phenotype);
+    return str_distance(being.phenotype, this.target.phenotype);
   }
   random_being(this) {
-    genome = random_genome(len(this.being.genotype));
-    return TextBeing(genome, this.being.encoding);
+    const genome = random_genome(this.target.genotype.length);
+    return TextBeing(genome, this.target.encoding);
   }
   alphabet(this) {
-    return list(set(this.being.phenotype));
+    return list(set(this.target.phenotype));
   }
 }
 
 function random_genome(size) {
-  // return bytearray(randint(0x00, 0xff) for _ in range(size))
+  const genome = [];
+  for (let i = 0; i < size; ++i) {
+    const random_byte = randint(0x00, 0xff);
+    genome.push(random_byte);
+  }
+  return genome;
 }
 
 function randint(min, max) {
   return parseInt(Math.round(Math.random() * (max - min) + min));
 }
+
+module.exports = { TextBeing, TextTarget };
