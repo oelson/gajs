84cef8cfc8dfa047958d28014293033385e3e0cc
Nelson Houillon <houillon.nelson@gmail.com>
2020-09-13 17:11:50 +0200

    replace decode_utf8 by Node's StringDecoder

--- src/species/utf8.js
+++ src/species/utf8.js
@@ -3,6 +3,9 @@ const {
   byte_vector_distance,
 } = require("../genetic_algorithm/selection");
 const random = require("random");
+const { StringDecoder } = require("string_decoder");
+
+const Utf8Decoder = new StringDecoder("utf8");
 
 function encode_utf8(s) {
   let i = 0,
@@ -39,52 +42,10 @@ function encode_utf8(s) {
   return bytes.subarray(0, i);
 }
 
-function decode_utf8(bytes) {
-  let i = 0,
-    s = "";
-  while (i < bytes.length) {
-    let c = bytes[i++];
-    if (c > 127) {
-      if (c > 191 && c < 224) {
-        if (i >= bytes.length)
-          throw new Error("UTF-8 decode: incomplete 2-byte sequence");
-        c = ((c & 31) << 6) | (bytes[i++] & 63);
-      } else if (c > 223 && c < 240) {
-        if (i + 1 >= bytes.length)
-          throw new Error("UTF-8 decode: incomplete 3-byte sequence");
-        c = ((c & 15) << 12) | ((bytes[i++] & 63) << 6) | (bytes[i++] & 63);
-      } else if (c > 239 && c < 248) {
-        if (i + 2 >= bytes.length)
-          throw new Error("UTF-8 decode: incomplete 4-byte sequence");
-        c =
-          ((c & 7) << 18) |
-          ((bytes[i++] & 63) << 12) |
-          ((bytes[i++] & 63) << 6) |
-          (bytes[i++] & 63);
-      } else
-        throw new Error(
-          "UTF-8 decode: unknown multibyte start 0x" +
-            c.toString(16) +
-            " at index " +
-            (i - 1)
-        );
-    }
-    if (c <= 0xffff) s += String.fromCharCode(c);
-    else if (c <= 0x10ffff) {
-      c -= 0x10000;
-      s += String.fromCharCode((c >> 10) | 0xd800);
-      s += String.fromCharCode((c & 0x3ff) | 0xdc00);
-    } else
-      throw new Error(
-        "UTF-8 decode: code point 0x" + c.toString(16) + " exceeds UTF-16 reach"
-      );
-  }
-  return s;
-}
-
 class Utf8Being {
   constructor(genotype) {
-    const phenotype = decode_utf8(genotype); // TODO python "replace" equivalent
+    const buffer = Buffer.from(genotype);
+    const phenotype = Utf8Decoder.end(buffer);
 
     this.initial_genotype = genotype.slice();
     this.genotype = genotype;
@@ -137,4 +98,4 @@ function random_text(length, alphabet) {
   return chars.join("");
 }
 
-module.exports = { Utf8Being, Utf8Target, encode_utf8, decode_utf8 };
+module.exports = { Utf8Being, Utf8Target, encode_utf8 };
