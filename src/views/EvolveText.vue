16f4f1772e0b1f3b8153eb135ba3cfc055b0ca88
Nelson Houillon <houillon.nelson@gmail.com>
2021-01-09 20:57:11 +0100

    text evolution skelton

--- src/views/EvolveText.vue
+++ src/views/EvolveText.vue
@@ -1,9 +1,52 @@
 <template>
-  <div>Evolve text !</div>
+  <div class="evolve">
+    <input
+      ref="tg"
+      type="text"
+      placeholder="Texte cible"
+      class="text"
+      v-model="targetText"
+      :readonly="running"
+    />
+    <button ref="go" @click="toggle">
+      <template v-if="running">Stop</template>
+      <template v-else>Go</template>
+    </button>
+    <span>{{ betterText }}</span>
+  </div>
 </template>
 
 <script>
 export default {
   name: "EvolveText",
+  data() {
+    return {
+      running: false,
+      targetText: "le cadavre exquis boira le vin nouveau",
+      betterText: "",
+    }
+  },
+
+  methods: {
+    toggle() {
+      this.running = !this.running
+    },
+  },
 }
 </script>
+
+<style lang="less" scoped>
+.evolve {
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: space-around;
+  align-items: center;
+
+  .text {
+    width: 100%;
+    padding: 10px;
+    margin: 10px 0;
+    box-sizing: border-box;
+  }
+}
+</style>
