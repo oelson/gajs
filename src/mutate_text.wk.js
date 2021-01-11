40cba534c531dbae94aeeab0fd08f825aa3d2e9e
Nelson Houillon <houillon.nelson@gmail.com>
2021-01-11 22:27:56 +0100

    web workers

---
+++ src/mutate_text.wk.js
@@ -0,0 +1,11 @@
+const { mutate_text } = require("./ga/text")
+
+onmessage = function(e) {
+  const generations = mutate_text(e.data)
+  for (const { rank, population } of generations) {
+    const best = population[population.length - 1]
+    const worst = population[0]
+    const population_length = population.length
+    postMessage({ rank, best, worst, population_length })
+  }
+}
--- src/views/EvolveText.vue
+++ src/views/EvolveText.vue
@@ -5,14 +5,22 @@
       type="text"
       placeholder="Texte cible"
       class="text"
-      v-model="targetText"
-      :readonly="running"
+      v-model="conf.target.text"
+      :readonly="worker !== undefined"
     />
     <button ref="go" @click="toggle">
-      <template v-if="running">Stop</template>
+      <template v-if="worker !== undefined">Stop</template>
       <template v-else>Go</template>
     </button>
-    <span>{{ betterText }}</span>
+
+    <span>
+      [{{ latest.rank }}] s:{{ latest.population_length }} p:{{
+        latest.best_survival
+      }}
+      -
+      {{ latest.worst_survival }}
+      b:"{{ latest.best.phenotype }}" (0x{{ latest.best.genotype_byte_string }})
+    </span>
   </div>
 </template>
 
@@ -21,15 +29,62 @@ export default {
   name: "EvolveText",
   data() {
     return {
-      running: false,
-      targetText: "le cadavre exquis boira le vin nouveau",
-      betterText: "",
+      conf: {
+        target: {
+          alphabet: "abcdefghijklmnopqrstuvwxyz ",
+          text: "les zebres sont des animaux tres fragiles",
+        },
+        start: {
+          length: 100,
+          function: "random_fixed_length",
+        },
+        reproduction: {
+          rate: 10,
+          function: "clone",
+        },
+        selection: {
+          evaluation: "evaluate_phenotype",
+          reduction: "keep_population_stable",
+        },
+        mutations: {
+          functions: {
+            insert_letter: 1,
+            remove_letter: 1,
+            replace_letter: 2,
+            insert_byte: 0,
+            remove_byte: 0,
+            replace_byte: 0,
+            alter_byte: 0,
+          },
+          number_per_cycle: 1,
+        },
+        stop: {
+          rank: 10000,
+          survival_p: 0.97,
+        },
+      },
+      worker: undefined,
+      latest: {
+        rank: 0,
+        population_length: 0,
+        best: "",
+        worst: "",
+      },
     }
   },
 
   methods: {
     toggle() {
-      this.running = !this.running
+      if (this.worker !== undefined) {
+        this.worker.terminate()
+        this.worker = undefined
+      } else {
+        this.worker = new Worker("/mutate_text.wk.umd.min.js")
+        this.worker.onmessage = (e) => {
+          this.latest = e.data
+        }
+        this.worker.postMessage(this.conf)
+      }
     },
   },
 }
