5b5f2c3873d47946c45a78eea61605d5f9f756da
Nelson Houillon <houillon.nelson@gmail.com>
2021-04-20 21:58:03 +0200

    remove unused code

--- src/ga/bytes.js
+++ src/ga/bytes.js
@@ -1,23 +1,5 @@
 const random = require("random")
 
-function flip_bit_in_byte(byte, bit_index) {
-  const mask = 1 << bit_index
-  const new_byte = byte ^ mask
-  return new_byte
-}
-
-function flip_bit_in_bytes(bytes, byte_index, bit_index) {
-  const original_byte = bytes[byte_index]
-  const new_byte = flip_bit_in_byte(original_byte, bit_index)
-  bytes[byte_index] = new_byte
-}
-
-function flip_random_bit_in_random_byte(bytes) {
-  const bit_index = random.int(0, 7)
-  const byte_index = random.int(0, bytes.length - 1)
-  flip_bit_in_bytes(bytes, byte_index, bit_index)
-}
-
 function insert_random_byte(bytes) {
   const new_byte = random.int(0x00, 0xff)
   const byte_index = random.int(0, bytes.length - 1)
@@ -45,7 +27,6 @@ function random_bytes(length) {
 }
 
 module.exports = {
-  flip_random_bit_in_random_byte,
   replace_random_byte,
   insert_random_byte,
   remove_random_byte,
